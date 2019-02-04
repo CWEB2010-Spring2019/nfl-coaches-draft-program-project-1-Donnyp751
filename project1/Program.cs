51a115a6d5a12a40934c164c797da8c34ce69489
Donald Posterick <donny.p@live.com>
2019-02-04 12:37:59 -0600

    Added first picks using program

--- project1/Program.cs
+++ project1/Program.cs
@@ -11,20 +11,17 @@ namespace project1
         {
             string dataFilePath = @"..\..\..\playerDataFile.dat";
 
-            Player p1 = new Player("Dwayne Haskins","Ohio St.", "Quarterback" , 24600100, 1 );
-            Player p2 = new Player("Josh Jacobs", "Alabama", "Quarterback", 24500100, 1);
-            List<Player> players = new List<Player>();
+            //Player p1 = new Player("Dwayne Haskins","Ohio St.", "Quarterback" , 24600100, 1 );
+            //Player p2 = new Player("Josh Jacobs", "Alabama", "Quarterback", 24500100, 1);
+            List<Player> players;// = new List<Player>();
 
             TextReader tReader = new StreamReader(dataFilePath);
             string serializedData = tReader.ReadToEnd();
             tReader.Close();
 
-            players.Add(p1);
-            players.Add(p2);
 
 
-
-            //players = JsonConvert.DeserializeObject<List<Player>>(serializedData);
+            players = JsonConvert.DeserializeObject<List<Player>>(serializedData);
 
             ConsoleKey sentienniel = ConsoleKey.A;
 
@@ -33,28 +30,41 @@ namespace project1
                 Console.WriteLine(p);
             }
 
-            while (sentienniel != ConsoleKey.Escape)
+            do
             {
                 Console.WriteLine("PlayerName, State, Position, Price, Pick");
-                var player = new Player(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())) ;
-                Console.Write(player);
+
+                Player player = null;
+                try
+                {
+                    player = new Player(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(),
+                        Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+                }
+                catch
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+
+                Console.WriteLine(player);
                 Console.WriteLine("Press up to confirm, down to reset");
                 if (Console.ReadKey().Key == ConsoleKey.UpArrow)
                 {
                     players.Add(player);
-                    Console.WriteLine("Player Added: total players =" + players.Count.ToString() );
+                    Console.WriteLine("Player Added: total players = " + players.Count.ToString() + "\n");
                 }
+
                 Console.WriteLine("press escape to quit, or any other key to continue adding players");
                 sentienniel = Console.ReadKey().Key;
-            }
+            } while (sentienniel != ConsoleKey.Escape);
 
             serializedData = JsonConvert.SerializeObject(players, Formatting.Indented);
             TextWriter tWriter = new StreamWriter(dataFilePath);
             tWriter.Write(serializedData);
 
             tWriter.Close();
-
-            Console.ReadKey();
+            
 
 
         }
