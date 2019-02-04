5558f058be96a3cf7d98319a01f48d865a70b552
Donald Posterick <donny.p@live.com>
2019-02-04 12:09:29 -0600

    Added entry loop for entering players into json

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace project1
 {
@@ -6,7 +9,56 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string dataFilePath = @"..\..\..\playerDataFile.dat";
+
+            Player p1 = new Player("Dwayne Haskins","Ohio St.", "Quarterback" , 24600100, 1 );
+            Player p2 = new Player("Josh Jacobs", "Alabama", "Quarterback", 24500100, 1);
+            List<Player> players = new List<Player>();
+
+            TextReader tReader = new StreamReader(dataFilePath);
+            string serializedData = tReader.ReadToEnd();
+            tReader.Close();
+
+            players.Add(p1);
+            players.Add(p2);
+
+
+
+            //players = JsonConvert.DeserializeObject<List<Player>>(serializedData);
+
+            ConsoleKey sentienniel = ConsoleKey.A;
+
+            foreach (Player p in players)
+            {
+                Console.WriteLine(p);
+            }
+
+            while (sentienniel != ConsoleKey.Escape)
+            {
+                Console.WriteLine("PlayerName, State, Position, Price, Pick");
+                var player = new Player(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())) ;
+                Console.Write(player);
+                Console.WriteLine("Press up to confirm, down to reset");
+                if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+                {
+                    players.Add(player);
+                    Console.WriteLine("Player Added: total players =" + players.Count.ToString() );
+                }
+                Console.WriteLine("press escape to quit, or any other key to continue adding players");
+                sentienniel = Console.ReadKey().Key;
+            }
+
+            serializedData = JsonConvert.SerializeObject(players, Formatting.Indented);
+            TextWriter tWriter = new StreamWriter(dataFilePath);
+            tWriter.Write(serializedData);
+
+            tWriter.Close();
+
+            Console.ReadKey();
+
+
         }
     }
+
+
 }
---
+++ project1/player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class Player
+    {
+        public string position { get; set; }
+        public int pick { get; set; } //1st is the best, 5th is the worst (of the best)
+        public int price { get; set; }
+        public string state { get; set; }
+        public string name { get; set; }
+
+        public Player(string _name, string _state, string _position, int price, int _pick)
+        {
+            this.name = _name;
+            this.state = _state;
+            this.position = _position;
+            this.price = price;
+            this.pick = _pick;
+        }
+        public Player()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "name:" + this.name + "  position: " + this.position + "  state: " + this.state + "   price: " + this.price + "   pick: " + this.pick; ;
+        }
+    }
+}
